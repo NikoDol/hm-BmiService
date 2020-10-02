50caaf268510a20267f0361ace417f0aa423771c
NikoDol <69104440+NikoDol@users.noreply.github.com>
2020-10-02 15:00:35 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
         int weight = 85;
         // Рост указываем в сантиметрах.
         int height = 1_75;
-        int index = service.calculate(weight, height);
+        double index = service.calculate(weight, height);
         System.out.println(index);
     }
 }
